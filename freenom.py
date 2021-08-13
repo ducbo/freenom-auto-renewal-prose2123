9af4aa8a7e9fe1e0efcb63ff31fd021469bc3f3a
Tran Dinh Duc <ducbo@yahoo.com>
2021-08-14 02:45:35 +0700

    Update freenom.py

--- freenom.py
+++ freenom.py
@@ -2,24 +2,18 @@ import re
 
 import requests
 
-# 登录地址
 LOGIN_URL = 'https://my.freenom.com/dologin.php'
 
-# 域名状态地址
 DOMAIN_STATUS_URL = 'https://my.freenom.com/domains.php?a=renewals'
 
-# 域名续期地址
 RENEW_DOMAIN_URL = 'https://my.freenom.com/domains.php?submitrenewals=true'
 
-# token 正则
 token_ptn = re.compile('name="token" value="(.*?)"', re.I)
 
-# 域名信息正则
 domain_info_ptn = re.compile(
     r'<tr><td>(.*?)</td><td>[^<]+</td><td>[^<]+<span class="[^<]+>(\d+?).Days</span>[^&]+&domain=(\d+?)">.*?</tr>',
     re.I)
 
-# 登录状态正则
 login_status_ptn = re.compile('<a href="logout.php">Logout</a>', re.I)
 
 
@@ -81,5 +75,5 @@ class FreeNom:
                     f'renewalperiod[{renewal_id}]': '12M',
                     'paymentmethod': 'credit'
                 })
-                print(domain, '续期成功' if r.text.find('Order Confirmation') != -1 else '续期失败')
-            print(f'{domain} 还有 {days} 天续期')
+                print(domain, 'done' if r.text.find('Order Confirmation') != -1 else 'done')
+            print(f'{domain} no {days} no')
